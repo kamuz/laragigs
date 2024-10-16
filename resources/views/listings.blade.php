791467cd84db9c79869ab51f932d48ddee3a2091
Volodymyr Kamuz <v.kamuz@gmail.com>
2024-10-16 19:30:51 +0000

    Rewrite view with blade syntax

--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -6,10 +6,10 @@
     <title>Document</title>
 </head>
 <body>
-<h1><?php echo $heading; ?></h1>
-    <?php foreach( $listings as $listing ): ?>
-        <h2><?php echo $listing['title']; ?></h2>
-        <p><?php echo $listing['description']; ?></p>
-    <?php endforeach; ?>
+<h1>{{$heading}}</h1>
+@foreach($listings as $listing)
+    <h2>{{$listing['title']}}</h2>
+    <p>{{$listing['description']}}</p>
+@endforeach
 </body>
 </html>
