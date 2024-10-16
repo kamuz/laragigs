faad9506d4c540275b17d74abae60492243ef432
Volodymyr Kamuz <v.kamuz@gmail.com>
2024-10-16 19:40:11 +0000

    Add condition if empty data

--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -7,9 +7,14 @@
 </head>
 <body>
 <h1>{{$heading}}</h1>
-@foreach($listings as $listing)
-    <h2>{{$listing['title']}}</h2>
-    <p>{{$listing['description']}}</p>
-@endforeach
+@if(count($listings) == 0)
+    <p>No listings found</p>
+@else
+    @foreach($listings as $listing)
+        <h2>{{$listing['title']}}</h2>
+        <p>{{$listing['description']}}</p>
+    @endforeach
+@endif
+
 </body>
 </html>
