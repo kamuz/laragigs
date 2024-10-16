6af83788f9076145380195d3fbe8f185fdc18b43
Volodymyr Kamuz <v.kamuz@gmail.com>
2024-10-16 19:25:17 +0000

    Add route and view for test data of listenings

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,19 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('listings', [
+        'heading' => "Latest Listings",
+        'listings' => [
+            [
+                'id' => 1,
+                'title' => 'Listing One',
+                'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Et blanditiis soluta nihil, molestiae pariatur beatae ipsam! Vero repudiandae ab nesciunt, soluta, quis assumenda, quos ex eveniet consequuntur ducimus, molestias dignissimos!'
+            ],
+            [
+                'id' => 2,
+                'title' => 'Listing Two',
+                'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Et blanditiis soluta nihil, molestiae pariatur beatae ipsam! Vero repudiandae ab nesciunt, soluta, quis assumenda, quos ex eveniet consequuntur ducimus, molestias dignissimos!'
+            ],
+        ]
+    ]);
 });
