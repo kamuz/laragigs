dd5086fb001a6ab0c9a8df654a731d86f58710d4
Volodymyr Kamuz <v.kamuz@gmail.com>
2024-10-17 23:26:49 +0000

    Filter listing by tag

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -9,8 +9,9 @@ class ListingController extends Controller
 {
     // Show all listings
     public function index() {
+        // dd(request('tag'));
         return view('listings.index', [
-            'listings' => Listing::all(),
+            'listings' => Listing::where('tags', 'like', '%' . request('tag') . '%')->get(),
         ]);
     }
 
