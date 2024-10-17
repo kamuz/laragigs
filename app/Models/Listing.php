03dc7d56fd0a872e4a6db975a24c70d2900ba89a
Volodymyr Kamuz <v.kamuz@gmail.com>
2024-10-17 13:04:25 +0000

    Rebuild model listing

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -1,28 +1,10 @@
 <?php
+
 namespace App\Models;
 
-class Listing {
-    public static function all(){
-        return [
-            [
-                'id' => 1,
-                'title' => 'Listing One',
-                'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Et blanditiis soluta nihil, molestiae pariatur beatae ipsam! Vero repudiandae ab nesciunt, soluta, quis assumenda, quos ex eveniet consequuntur ducimus, molestias dignissimos!'
-            ],
-            [
-                'id' => 2,
-                'title' => 'Listing Two',
-                'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Et blanditiis soluta nihil, molestiae pariatur beatae ipsam! Vero repudiandae ab nesciunt, soluta, quis assumenda, quos ex eveniet consequuntur ducimus, molestias dignissimos!'
-            ],
-        ];
-    }
+use Illuminate\Database\Eloquent\Model;
 
-    public static function find($id){
-        $listings = self::all();
-        foreach($listings as $listing){
-            if($listing['id'] == $id) {
-                return $listing;
-            }
-        }
-    }
+class Listing extends Model
+{
+    //
 }
