e3fcf6be67858d71d37e35d01a56635ca972bc3a
Volodymyr Kamuz <v.kamuz@gmail.com>
2024-10-20 12:22:04 +0300

    Rebuild search with scopeFilter

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -10,10 +10,18 @@ class Listing extends Model
     use HasFactory;
 
     public function scopeFilter($query, array $filters) {
+
         // Show posts by tag
         if($filters['tag'] ?? false){
             // dd($filters['tag']);
             $query->where('tags', 'like', '%' . request('tag') . '%');
+        }
+
+        // Seach request
+        if($filters['search'] ?? false){
+            // dd($filters['search']);
+            $query->where('title', 'like', '%' . request('search') . '%')
+                ->orWhere('tags', 'like', '%' . request('search') . '%');
         }
     }
 }
